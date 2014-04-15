3eab14e834b77d08d115877dbf8d9bcfde7016e8
bugy <buggygm@gmail.com>
2014-04-15 20:14:47 +0400

    added column type _NullFlags (for FoxPro databases) added catching of NumericOverflow (for several databases), i.e. asterisk (*) characters improved getting String (now its better trimmed from both sides) added loading Integer value

--- src/net/iryndin/jdbf/core/DbfFieldTypeEnum.java
+++ src/net/iryndin/jdbf/core/DbfFieldTypeEnum.java
@@ -1,39 +1,40 @@
 package net.iryndin.jdbf.core;
 
 public enum DbfFieldTypeEnum {
-	Character('C'),
-	Currency('Y'),
-	Numeric('N'),
-	Float('F'),
-	Date('D'),
-	DateTime('T'),
-	Double('B'),
-	Integer('I'),
-	Logical('L'),
-	Memo('M'),
-	General('G'),
-	Picture('P');
-	
-	final char type;
-	
-	DbfFieldTypeEnum(char type) {
-		this.type= type;
-	}
-	
-	public static DbfFieldTypeEnum fromChar(char type) {
-		for (DbfFieldTypeEnum e : DbfFieldTypeEnum.values()) {
-			if (e.type == type) {
-				return e;
-			}
-		}
-		return null;
-	}
-	
-	public byte toByte() {
-		return (byte)type;
-	}
-	
-	public char getType() {
-		return type;
-	}
+    Character('C'),
+    Currency('Y'),
+    Numeric('N'),
+    Float('F'),
+    Date('D'),
+    DateTime('T'),
+    Double('B'),
+    Integer('I'),
+    Logical('L'),
+    Memo('M'),
+    General('G'),
+    Picture('P'),
+    NullFlags('0');
+
+    final char type;
+
+    DbfFieldTypeEnum(char type) {
+        this.type = type;
+    }
+
+    public static DbfFieldTypeEnum fromChar(char type) {
+        for (DbfFieldTypeEnum e : DbfFieldTypeEnum.values()) {
+            if (e.type == type) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+    public byte toByte() {
+        return (byte) type;
+    }
+
+    public char getType() {
+        return type;
+    }
 }
